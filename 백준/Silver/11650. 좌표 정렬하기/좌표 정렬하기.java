33556b9ea74a5fc41ffe72f0edae5fa708f3c1cd
sohoneyee <139411346+sohoneyee@users.noreply.github.com>
2023-10-22 10:01:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2544 ms, Memory: 77040 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] coor = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			coor[i][0] = Integer.parseInt(st.nextToken());
+			coor[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		// x좌표 오름차순 -> 같으면 y좌표 오름차순
+		Arrays.sort(coor, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] > o2[0]) return 1;
+				else if (o1[0] < o2[0]) return -1;
+				else return o1[1] - o2[1];
+			}
+		});
+
+		for (int[] arr : coor) {
+			for (int k : arr)
+				System.out.print(k + " ");
+			System.out.println();
+		}
+	} // main
+}
